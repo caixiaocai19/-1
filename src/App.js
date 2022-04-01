a6d0f19d7fa74f775baf2dfb9330474c28ab7354
caixiaocai19 <2945903048@qq.com>
2022-04-02 00:31:23 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import MonacoEditor from 'react-monaco-editor';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const defaultCode = `export default {
+  name: 'name',
+  code: 'code'
+}`;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      code: defaultCode
+    }
+    this.onChangeHandle = this
+      .onChangeHandle
+      .bind(this);
+  }
+  onChangeHandle(value, e) {
+    this.setState({code: value});
+  }
+  editorDidMountHandle(editor, monaco) {
+    console.log('editorDidMount', editor);
+    editor.focus();
+  }
+  render() {
+    const code = this.state.code;
+    const options = {
+      selectOnLineNumbers: true,
+      renderSideBySide: false
+    };
+    return (
+      <div >
+        <div className="wrapper">
+          <div className="editor-container">
+            <MonacoEditor
+              theme="vs-dark"
+              language="javascript"
+              value={code}
+              options={options}
+              onChange={this.onChangeHandle}
+              editorDidMount={this.editorDidMountHandle}/>
+          </div>
+          <div className="view" contenteeitable={'true'}>
+            {this.state.code}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
